e91901a12b431e8cc2cf73d1d680694c555e4037
Yogesh Chaudhari <YChaudhari@linz.govt.nz>
2021-08-21 10:50:53 +1200

    fixes fixture

--- fixtures/fixture.ts
+++ fixtures/fixture.ts
@@ -2,10 +2,10 @@ import { test as baseTest } from '@playwright/test';
 import LandingPage from '../models/landing.page';
 
 export const test = baseTest.extend<{ landingPage: LandingPage }>({
-  landingPage: (async ({ page }) => {
+  landingPage: (async ({ page }, use) => {
     const landingPage = new LandingPage(page);
     await landingPage.navigate();
-    return landingPage;
+    use(landingPage);
   }),
 });
 
